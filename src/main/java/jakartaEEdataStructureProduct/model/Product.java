8482b7d9f916e26d7e1df2f7c08627cc5e3846e6
Zelhad <ze.elhadraoui@gmail.com>
2025-11-04 04:31:36 +0100

    Add a very basic Object  product

---
+++ src/main/java/jakartaEEdataStructureProduct/model/Product.java
@@ -0,0 +1,32 @@
+package jakartaEEdataStructureProduct.model;
+
+public class Product {
+	private String code;
+	private String name;
+
+	public Product() {
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Product(String code, String name) {
+		this.code = code;
+		this.name = name;
+	}
+	
+
+}
